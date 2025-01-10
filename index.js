a4cd34b1558d7ee51a3983a0d63eaf800b611906
Bryan Luu <loc.bryan.luu@gmail.com>
2025-01-10 13:37:24 -0800

    :sparkles: add basic computation for tax
    Doesn't match TurboTax though...

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 var income_input = document.querySelector(".income-dollar-amount > .input-value");
 var tax_value = document.querySelector(".income-tax-calculated > .calculated-value");
 
+const TAX_BRACKET_BOUNDARIES = [42279, 98560, 113158, 137407, 186306, 259829];
+const TAX_BRACKET_RATES = [5.06, 7.70, 10.50, 12.29, 14.70, 16.80, 20.50];
+
+/**
+ *
+ * @param {Number} income Taxable income
+ * @returns After-tax income according to https://www2.gov.bc.ca/gov/content/taxes/income-taxes/personal/tax-rates
+ */
 function computeIncomeTax(income) {
-  return income; // dummy for now
+  var tax = 0;
+  var lastBoundary = 0;
+  // check each boundary, applying tax as suitable
+  for (var i = 0; i < TAX_BRACKET_BOUNDARIES.length; i++) {
+    var boundary = TAX_BRACKET_BOUNDARIES[i];
+    var rate = TAX_BRACKET_RATES[i] / 100;
+    if (income > boundary) {
+      // compute cumulative tax, move to next boundary
+      tax += rate * (boundary - lastBoundary);
+      lastBoundary = boundary;
+    } else { // if income falls within a boundary
+      // compute final cumulate tax and return it
+      tax += rate * (income - lastBoundary);
+      return tax;
+    }
+  }
+  // if here, income is in the highest bracket
+  rate = TAX_BRACKET_RATES[TAX_BRACKET_BOUNDARIES.length] / 100;
+  tax += rate * (income - lastBoundary);
+  return tax;
 }
 
 function updateDisplayedTax(computedTax) {
@@ -13,3 +40,5 @@ income_input.addEventListener("change", (event) => {
   var newValue = event.target.value;
   updateDisplayedTax(computeIncomeTax(newValue));
 })
+
+updateDisplayedTax(income_input.value);
